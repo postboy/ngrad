08459d98fe395f4596b5747efd02fe9646825296
Ivan Zuboff <postboy@users.noreply.github.com>
2024-01-03 16:50:30 +0300

    simplify get-screen

--- src/kaliningrad/term.clj
+++ src/kaliningrad/term.clj
@@ -1,18 +1,9 @@
 (ns kaliningrad.term
-  (:import java.nio.charset.Charset
-           com.googlecode.lanterna.TerminalFacade
-           com.googlecode.lanterna.screen.Screen
-           com.googlecode.lanterna.terminal.Terminal
-           com.googlecode.lanterna.input.Key)
-  (:require [lanterna.terminal :as t]))
+  (:require [lanterna.screen :as s]))
 
 (defn get-screen
-  ([] (get-screen 20 20 identity))
-  ([cols rows resized-fn]
-   (let [terminal
-         (TerminalFacade/createTerminal (Charset/forName "UTF-8"))
-         #_(TerminalFacade/createSwingTerminal cols rows)
-         screen (new Screen terminal)]
-     (.startScreen screen)
-     (t/add-resize-listener terminal resized-fn)
-     screen)))
+  [cols rows resized-fn]
+  (let [screen (s/get-screen :auto)]
+    (s/start screen)
+    (s/add-resize-listener screen resized-fn)
+    screen))
