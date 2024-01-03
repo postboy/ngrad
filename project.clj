2fe18c518aa26fe5998454154084308c1c24eabb
Ivan Zuboff <postboy@users.noreply.github.com>
2024-01-04 01:00:39 +0300

    bump Clojure version
    - lein repl wants at least 1.7.0
    - cider-nrepl wants at least 1.8.0

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A simple roguelike not-quite game. Go for a walk on Russian city of Kaliningrad!"
   :url "https://github.com/postboy/kaliningrad"
   :license {:name "MIT/X11"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-lanterna "0.9.7"]]
   :main kaliningrad.core)
