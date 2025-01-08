1bcb930f755702341301cc6c40455053cfc9c39f
Ivan Zuboff <postboy@users.noreply.github.com>
2025-01-08 23:10:11 +0300

    bump version for using clojure.math

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A simple roguelike not-quite game. Go for a walk through the city streets!"
   :url "https://github.com/postboy/ngrad"
   :license {:name "MIT/X11"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
                  [clojure-lanterna "0.9.7"]]
   :main ngrad.core)
